b454ec504e45aaaffc4c71108aee74afa10107fc
I-GIKARU <aizohgikaru@gmail.com>
2025-03-12 21:12:09 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = "Grace Hopper";
+
+// Define the welcomeMessage using template literals
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define the excitedWelcomeMessage by converting welcomeMessage to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Define the shortGreeting using only the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
